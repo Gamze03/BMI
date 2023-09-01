ce09f944bfa33c518100194085852e206bb3273b
qemze <qemzemirkisiyeva90@gmail.com>
2023-09-01 17:54:58 +0400

    alt kateqoriya elave edildi

--- swiper.js
+++ swiper.js
@@ -122,3 +122,25 @@ const PartnersSwiper = new Swiper(".partners-swiper", {
   
     }
 );
+
+
+const TrainingsSwiper = new Swiper(".trainings-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    speed: 1000,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    }
+);
